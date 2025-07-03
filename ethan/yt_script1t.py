a1562bcf82e7625656aba3fbcd351bc2da93ab2b
Ethan Tang <ethanalextang@gmail.com>
2025-07-03 12:18:53 -0700

    Update yt_script1t.py

--- ethan/yt_script1t.py
+++ ethan/yt_script1t.py
@@ -1,7 +1,7 @@
 import yt
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-ds = yt.load("./sims_data/R1.5_v2400_b250/Data_000005")
+ds = yt.load("./sims_data/R1.5_v2400_b250/Data_000044")
 sc = yt.create_scene(ds, lens_type="perspective")
 source = sc[0]
 bounds = (5e-30, 5e-27)
